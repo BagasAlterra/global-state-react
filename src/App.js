7da698613adeb36681976faf7624f7affc089c8d
bagasalterra <bagas.dhitya@alterra.id>
2022-09-05 10:30:05 +0700

    add carousel

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 import Rectangle from "./components/Rectangle";
 import { Button } from "react-bootstrap";
 import CustomModal from "./components/CustomModal";
+import CustomCarousel from "./components/CustomCarousel";
 
 const App = () => {
   const [color, setColor] = useState("red");
   const [modal, setModal] = useState(false);
+
+  const dataImages = {
+    firstSlide:
+      "https://cdn0-production-images-kly.akamaized.net/VfzViwLYTNFU4-QXPBy4aa2lRL4=/0x587:6016x3978/640x360/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3797087/original/003568800_1640615228-thomas-iversen-4W8FgDVyUME-unsplash.jpg",
+    secondSlide:
+      "https://i0.wp.com/starfarm.co.id/wp-content/uploads/2021/07/bebek-peking.jpeg?fit=1278%2C897&ssl=1",
+    thirdSlide:
+      "https://asset.kompas.com/crops/KghJ6qUGRrpm0fK4EJICOXqspn8=/19x5:960x633/750x500/data/photo/2022/01/05/61d54b808e254.jpg",
+  };
 
   return (
     <div>
@@ -24,6 +34,13 @@ const App = () => {
           body={"Apa kamu yakin mau Menutup?"}
           handleShow={() => setModal(true)}
           handleClose={() => setModal(false)}
+        />
+      </div>
+      <div>
+        <CustomCarousel
+          imageFirst={dataImages.firstSlide}
+          imageSecond={dataImages.secondSlide}
+          imageThird={dataImages.thirdSlide}
         />
       </div>
     </div>
---
+++ src/components/CustomCarousel.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+const CustomCarousel = (props) => {
+  return (
+    <Carousel
+      style={{
+        width: 550,
+        height: 550,
+      }}
+    >
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={props.imageFirst}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={props.imageSecond}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={props.imageThird}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default CustomCarousel;
